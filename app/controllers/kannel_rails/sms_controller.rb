4bee6067e7046a904dd04777550f623d3a382cbb
Heinrich Lee Yu <hleeyu@gmail.com>
2012-09-25 16:25:15 +0800

    Check api_secret when receiving SMS

--- app/controllers/kannel_rails/sms_controller.rb
+++ app/controllers/kannel_rails/sms_controller.rb
@@ -2,18 +2,22 @@ module KannelRails
   class SmsController < ApplicationController
 
     def receive_message
-      reply = ""
+      if KannelRails.config.api_secret and KannelRails.config.api_secret != params[:api_secret]
+        render :status => :forbidden, :text => "Unauthorized Request"
+      else
+        reply = ""
 
-      KannelRails::Handlers.each do |handler_class|
-        handler = handler_class.new(params[:from], params[:text])
+        KannelRails::Handlers.each do |handler_class|
+          handler = handler_class.new(params[:from], params[:text])
 
-        if handler.handle?
-          reply = handler.invoke
-          break
+          if handler.handle?
+            reply = handler.invoke
+            break
+          end
         end
-      end
 
-      render :text => reply
+        render :text => reply
+      end
     end
 
   end
--- spec/controllers/sms_controller_spec.rb
+++ spec/controllers/sms_controller_spec.rb
@@ -3,8 +3,13 @@ require "spec_helper"
 describe KannelRails::SmsController do
 
   it "should echo Hello" do
-    get :receive_message, :text => "ECHO Hello", :use_route => :kannel_rails
+    get :receive_message, :text => "ECHO Hello", :api_secret => "test", :use_route => :kannel_rails
     response.body.should == "Hello"
+  end
+
+  it "should not allow unauthorized calls" do
+    get :receive_message, :text => "ECHO Hello", :use_route => :kannel_rails
+    response.status.should == 403
   end
 
 end

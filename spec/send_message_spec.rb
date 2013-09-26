8946ce804d9dd761cc17672193538a663df760cc
Heinrich Lee Yu <hleeyu@gmail.com>
2013-09-26 16:16:29 +0800

    Add test for passing extra parameters to kannel

--- spec/send_message_spec.rb
+++ spec/send_message_spec.rb
@@ -13,4 +13,18 @@ describe KannelRails do
     KannelRails.send_message("09101111111", "Hello World!")
   end
 
+  context "with extra params" do
+    it "should pass params to kannel" do
+      stub_request(:get, "#{KannelRails.config.kannel_url}:#{KannelRails.config.sendsms_port}/cgi-bin/sendsms?" +
+                         "dlr-mask=31&password=#{KannelRails.config.password}" +
+                         "&from=customsender" +
+                         "&text=Hello%20World!&to=09101111111" +
+                         "&username=#{KannelRails.config.username}").
+        with(:headers => {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+        to_return(:status => 200, :body => "", :headers => {})
+
+      KannelRails.send_message("09101111111", "Hello World!", :from => 'customsender')
+    end
+  end
+
 end
